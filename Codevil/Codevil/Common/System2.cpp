c3a2009dd6b73e12f40390e1a107dfbc855947a1
stackprobe <stackprobes@gmail.com>
2019-04-18 01:41:33 +0900

    daily

--- Codevil/Codevil/Common/System2.cpp
+++ Codevil/Codevil/Common/System2.cpp
@@ -1,4 +1,4 @@
 #include "all.h"
 
-oneObject(finalizers, new finalizers(), GetEndProcFinalizers);
-oneObject(finalizers, new finalizers(), GetFinalizers);
+oneObject(finalizers, new finalizers(), GetEndProcFinalizers); // EndProc()     で Flish() する。DxLib_End(); より前。error(); のとき呼ばれない。
+oneObject(finalizers, new finalizers(), GetFinalizers);        // termination() で Flish() する。DxLib_End(); より後。error(); のとき呼ばれる。
