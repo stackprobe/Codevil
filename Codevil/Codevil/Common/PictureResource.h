6eda06d6cabc701d10c233bcc5a6bbd165a58942
stackprobe <stackprobes@gmail.com>
2019-07-25 23:51:47 +0900

    Sunny

--- Codevil/Codevil/Common/PictureResource.h
+++ Codevil/Codevil/Common/PictureResource.h
@@ -1,3 +1,7 @@
 oneObjectProto(resCluster<PicInfo_t *>, GetStdPicRes);
 oneObjectProto(resCluster<PicInfo_t *>, GetInvPicRes);
 oneObjectProto(resCluster<PicInfo_t *>, GetMirrorPicRes);
+
+// app > @ original PicRes
+
+// < app
